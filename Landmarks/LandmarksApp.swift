cc2299f4a71e6e9051648fb93800381c6b98f28e
Dan Adler <2274667+dadler64@users.noreply.github.co>
2021-01-19 17:58:42 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dan on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
