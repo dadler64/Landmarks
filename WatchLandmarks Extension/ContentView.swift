f99629977ce82f0b3ed95f0687e4646f7b46f6dd
Dan Adler <2274667+dadler64@users.noreply.github.com>
2021-02-11 17:47:04 -0500

    Initial commit

--- WatchLandmarks Extension/ContentView.swift
+++ WatchLandmarks Extension/ContentView.swift
@@ -1,21 +1,21 @@
 //
 //  ContentView.swift
-//  Landmarks
+//  WatchLandmarks Extension
 //
-//  Created by Dan on 1/19/21.
+//  Created by Dan on 2/9/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
---
+++ WatchLandmarks Extension/LandmarkList.swift
@@ -0,0 +1,51 @@
+//
+//  LandmarkList.swift
+//  WatchLandmarks Extension
+//
+//  Created by Dan on 2/9/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(
+            [
+                "iPhone SE"
+//                ,"iPad mini 4"
+            ], id: \.self) { deviceName in
+            LandmarkList()
+                .environmentObject(ModelData())
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
+
